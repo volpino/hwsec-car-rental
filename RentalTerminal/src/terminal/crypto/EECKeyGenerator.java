1a4bf8338977ec5dadbfd750936559d5591261b1
Alessio Parzian <aparzian@gmail.com>
2014-10-19 21:29:11 +0200

    Save and store keys is now working -- keys folder created

--- RentalTerminal/src/terminal/crypto/EECKeyGenerator.java
+++ RentalTerminal/src/terminal/crypto/EECKeyGenerator.java
@@ -1,11 +1,14 @@
 package terminal.crypto;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.security.*;
 import java.security.spec.ECGenParameterSpec;
-
-//Temporary these libraries are not used
-//import org.bouncycastle.jce.ECNamedCurveTable;
-//import org.bouncycastle.jce.spec.ECParameterSpec;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 public class EECKeyGenerator {
 
@@ -36,21 +39,70 @@ public class EECKeyGenerator {
 		return pair.getPrivate();
 	}
 	
-	// Static method to be used in the graphic interface
+	public void setKeyPair(KeyPair pair){
+		this.pair = pair;
+	}
+	
 	public static KeyPair generateKeys() throws Exception{
 		EECKeyGenerator gen = new EECKeyGenerator();
 		KeyPair sc = gen.getKeyPair();
 		return sc;
 	}
+	
+	public static void saveKeys(KeyPair keys, String path, String carID) throws IOException{
+		PrivateKey privateKey = keys.getPrivate();
+		PublicKey publicKey = keys.getPublic();
+		// Store Public Key.
+		X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(publicKey.getEncoded());
+		FileOutputStream fos = new FileOutputStream(path+"/"+carID+".pub");
+		fos.write(x509EncodedKeySpec.getEncoded());
+		fos.close();
+		// Store Private Key.
+		PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(privateKey.getEncoded());
+		fos = new FileOutputStream(path+"/"+carID);
+		fos.write(pkcs8EncodedKeySpec.getEncoded());
+		fos.close();
+	}
+	
+	public static KeyPair loadKeys(String algorithm, String path, String carID) throws IOException, NoSuchAlgorithmException,
+	InvalidKeySpecException {
+		// Read Public Key
+		File filePublicKey = new File(path+"/"+carID+".pub");
+		FileInputStream fis = new FileInputStream(path+"/"+carID+".pub");
+		byte[] encodedPublicKey = new byte[(int) filePublicKey.length()];
+		fis.read(encodedPublicKey);
+		fis.close();
+		// Read Private Key
+		File filePrivateKey = new File(path+"/"+carID);
+		fis = new FileInputStream(path+"/"+carID);
+		byte[] encodedPrivateKey = new byte[(int) filePrivateKey.length()];
+		fis.read(encodedPrivateKey);
+		fis.close();
+		// Regenerate KeyPair.
+		KeyFactory keyFactory = KeyFactory.getInstance(algorithm);
+		X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(encodedPublicKey);
+		PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
+		PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(encodedPrivateKey);
+		PrivateKey privateKey = keyFactory.generatePrivate(privateKeySpec);
+		return new KeyPair(publicKey, privateKey);
+	}
 
-	// Testing purpose
 	public void printPair() {
 		System.out.println(pair.getPrivate());
 		System.out.println(pair.getPublic());
 	}
+	
+	// Testing purpose
+	// NOTE remember in the GUI to catch and manage "File already exists" exception -- now it is overwritten
 	public static void main(String[] args) {
 		try {
 			EECKeyGenerator gen = new EECKeyGenerator();
+			System.out.println("Keys generated");
+			gen.printPair();
+			saveKeys(gen.getKeyPair(),"keys","BW129SZ");
+			KeyPair test = loadKeys("ECDSA","keys","BW129SZ");
+			gen.setKeyPair(test);
+			System.out.println("Keys restored");
 			gen.printPair();
 		} catch (IllegalStateException e) {
 			// TODO Auto-generated catch block
@@ -68,6 +120,5 @@ public class EECKeyGenerator {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-	
+	}	
 }
